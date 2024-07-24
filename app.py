463067b3d3fbe298194772239b4c31f3ff414d15
lequyan2003 <lequyan2003@gmail.com>
2024-07-24 16:35:06 -0400

    integrate llmops _ 1

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-import openai
+import base64
+import os
+import tempfile
+
+import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI()
 
 print("ok")
