ca043f0da2d6c1745fcb190204706b966b264874
lequyan2003 <lequyan2003@gmail.com>
2024-07-28 00:41:57 -0400

    streamlit added _ 2

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI()
 
-# print(sample_prompt)
-
 # Initialize session state variables
 if "uploaded_file" not in st.session_state:
     st.session_state.uploaded_file = None
@@ -25,7 +23,7 @@ if "result" not in st.session_state:
 
 def encode_image(image_path):
     with open(image_path, 'rb') as image_file:
-        return base64.b64encode(image_file.read().decode('utf-8'))
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 
 def call_gpt4_model_for_analysis(file_name: str, sample_prompt=sample_prompt):
@@ -51,7 +49,7 @@ def call_gpt4_model_for_analysis(file_name: str, sample_prompt=sample_prompt):
     ]
 
     response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
+        model="gpt-3.5-turbo-0125",
         messages=messages,
         max_tokens=1500
     )
@@ -69,3 +67,26 @@ uploaded_file = st.file_uploader(
     "Upload an image",
     type=['jpg', 'jpeg', 'png']
 )
+
+# Temporary file handling
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=os.path.splitext(uploaded_file.name)[1]
+    ) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        st.session_state["filename"] = tmp_file.name
+
+    st.image(uploaded_file, caption='Uploaded Image')
+
+# Process Button
+if st.button("Analyze Image"):
+    if (
+        'filename' in st.session_state and
+        os.path.exists(st.session_state['filename'])
+    ):
+        st.session_state['result'] = call_gpt4_model_for_analysis(
+            st.session_state['filename']
+        )
+        st.markdown(st.session_state['result'], unsafe_allow_html=True)
+        os.unlink(st.session_state['filename'])  # Del tempfile after process
